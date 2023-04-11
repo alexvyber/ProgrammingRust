94f097aadc10aa51091b9f09f3269b7529a1c635
Alexey Sokolov <alexvyber@gmail.com>
2023-04-11 07:46:39 +0300

    finished 4 chapter

--- working/hello/src/main.rs
+++ working/hello/src/main.rs
@@ -45,7 +45,7 @@ fn main() {
     let v: Vec<f64> = vec![0.0, 0.707, 1.0, 0.707];
     let a: [f64; 4] = [0.0, -0.707, -1.0, -0.707];
     let sv: &[f64] = &v;
-    let sa: &[f64] = &a;
+    let _sa: &[f64] = &a;
 
     print(&v[0..2]); // print the first two elements of v
     print(&a[2..]); // print elements of a starting with a[2]
@@ -65,5 +65,57 @@ followed immediately by three pound signs ('###'):
     let mut s = "Govinda".to_string();
     let t = s;
     s = "Siddhartha".to_string(); // nothing is dropped here
-    println!("{}, {}", t, s)
+    println!("{}, {}", t, s);
+
+    // Build a vector of the strings "101", "102", ... "105"
+    let mut v = Vec::new();
+    for i in 101..106 {
+        v.push(i.to_string());
+    }
+    // 1. Pop a value off the end of the vector:
+    let fifth = v.pop().expect("vector empty!");
+    assert_eq!(fifth, "105");
+    // 2. Move a value out of a given index in the vector,
+    // and move the last element into its spot:
+    let second = v.swap_remove(1);
+    assert_eq!(second, "102");
+    // 3. Swap in another value for the one we're taking out:
+    let third = std::mem::replace(&mut v[2], "substitute".to_string());
+    assert_eq!(third, "103");
+    // Let's see what's left of our vector.
+    assert_eq!(v, vec!["101", "104", "substitute"]);
+
+    let v = vec![
+        "liberté".to_string(),
+        "égalité".to_string(),
+        "fraternité".to_string(),
+    ];
+    for mut s in v {
+        s.push('!');
+        println!("{}", s);
+    }
+
+    #[derive(Copy, Clone)]
+    struct Label {
+        number: u32,
+    }
+
+    // #[derive(Copy, Clone)]
+    struct StringLabel {
+        name: String,
+    }
+
+    fn print_struct(l: Label) {
+        println!("STAMP: {}", l.number);
+    }
+
+    let l = Label { number: 3 };
+    print_struct(l);
+    println!("My label number is: {}", l.number);
+
+    use std::rc::Rc;
+    // Rust can infer all these types; written out for clarity
+    let s: Rc<String> = Rc::new("shirataki".to_string());
+    let t: Rc<String> = s.clone();
+    let u: Rc<String> = s.clone();
 }
