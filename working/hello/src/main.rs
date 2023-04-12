2f11f7c8858da922ba19c23ffcab11a65c7c70b5
Alexey Sokolov <alexvyber@gmail.com>
2023-04-12 22:21:58 +0300

    page 148(Function and Method Calls)

--- working/hello/src/main.rs
+++ working/hello/src/main.rs
@@ -218,6 +218,22 @@ followed immediately by three pound signs ('###'):
     }
 
     static WORTH_POINTING_AT: i32 = 1000;
-    
+
     f(&WORTH_POINTING_AT);
+
+    struct S<'a, 'b> {
+        x: &'a i32,
+        y: &'b i32,
+    }
+
+    let x = 10;
+    let r;
+    {
+        let y = 20;
+        {
+            let s = S { x: &x, y: &y };
+            r = s.x;
+        }
+    }
+    println!("{}", r);
 }
